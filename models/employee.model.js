b0d4dda3a6832ceae1abfaf82dc6e0a0458e0ebb
Ernest Irabor <irabeny89@gmail.com>
2021-05-04 08:20:50 +0100

    employee object model

---
+++ models/employee.model.js
@@ -0,0 +1,11 @@
+class Employee {
+  constructor(employeeId, firstName, lastName, age, joinDate) {
+    this.employeeId = employeeId
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.joinDate = joinDate
+  }
+}
+
+module.exports = Employee
