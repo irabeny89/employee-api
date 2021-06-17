5da0f9848dc41fd462236c664058824662425382
Ernest Irabor <irabeny89@gmail.com>
2021-06-17 23:20:21 +0100

    add template for home route

--- express.js
+++ express.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const helmet = require('helmet')
 
 const employeeRoutes = require('./routes/employee.routes')
+const template = require('./template')
 
 const app = express()
 app.use(morgan('dev'))
@@ -14,9 +15,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (_req, res) => res.status(200).send(template()))
 app.use('/', employeeRoutes)
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
   if (err) return res.status(400).json(err)
 })
 
---
+++ template/index.js
@@ -0,0 +1,66 @@
+module.exports = () => `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HOME | Employee API</title>
+</head>
+<body>
+  <h1>Welcome to Employee API Home Route</h1><hr />
+  <h2><u>API Routes / Endpoints</u></h2>
+  <p>An API for Employee management with CRUD functions</p>
+  <h3><u>All Employees</u></h3>
+  <ul>
+    <li>
+      <code>GET http://localhost:3000/api/employees</code>
+    </li>
+  </ul>
+  <h3><u>Create an Employee</u></h3>
+  <ul>
+    <li>
+      <code>
+        <pre>
+          POST http://localhost:3000/api/employees
+          Content-Type: application/json
+
+          {
+            "firstName": "{{firstName}}",
+            "lastName": "{{lastName}}",
+            "age": "{{age}}"
+          }
+        </pre>
+      </code>
+    </li>
+  </ul>
+  <h3><u>Read Employee Data</u></h3>
+  <ul>
+    <li>
+      <code>GET http://localhost:3000/api/employees/:employeeId</code>
+    </li>
+  </ul>
+  <h3><u>Update Employee Data</u></h3>
+  <ul>
+    <li>
+      <code>
+        <pre>
+          PUT http://localhost:3000/api/employees/:employeeId
+          Content-Type: application/json
+          
+          {
+            "employeeId": "{{id1}}",
+            "firstName": "{{newFirstName}}",
+            "lastName": "{{newLastName}}"
+          }
+        </pre>
+      </code>
+    </li>
+  </ul>
+  <h3><u>Delete an Employee Data</u></h3>
+  <ul>
+    <li>
+      <code>DELETE http://localhost:3000/api/employees/:employeeId</code>
+    </li>
+  </ul>
+</body>
+</html>`
