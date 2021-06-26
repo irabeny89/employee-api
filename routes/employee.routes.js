97722f966df37dd8432e04b98eb9536391ca41a5
Ernest Irabor <irabeny89@gmail.com>
2021-06-26 11:06:19 +0100

    apicache implementation

--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -1,14 +1,20 @@
 const { Router } = require('express')
+const apicache = require('apicache').options({
+  headers: {
+    'cache-control': 'no-cache',
+  },
+}).middleware
+
 const { getAll, create, read, update, remove, employeeById } = require('../controllers/employee.controllers')
 
 const router = Router()
 
 router.route('/api/employees')
-  .get(getAll)
+  .get(apicache("30 seconds"), getAll)
   .post(create)
 
 router.route('/api/employees/:employeeId')
-  .get(read)
+  .get(apicache("30 seconds"), read)
   .put(update)
   .delete(remove)
 
